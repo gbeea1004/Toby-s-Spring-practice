fdfec3797c780cf15e4fea7cc13e61ab0b3e49c4
geonHee <corqkrtk3@naver.com>
2019-04-03 00:29:27 +0900

    JDBC INSERT 작성 시 DB 데이터 한글 깨짐현상 해결

--- src/main/java/com/tobyspring/tobyspring/domain/user/UserDao.java
+++ src/main/java/com/tobyspring/tobyspring/domain/user/UserDao.java
@@ -6,7 +6,7 @@ public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/springbook?serverTimezone=UTC", "root", "1234");
+                "jdbc:mysql://localhost:3306/springbook?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=utf8", "root", "1234");
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
@@ -23,7 +23,7 @@ public class UserDao {
     public User get(String id) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/springbook?serverTimezone=UTC", "root", "1234");
+                "jdbc:mysql://localhost:3306/springbook?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=utf8", "root", "1234");
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
         ps.setString(1, id);
@@ -33,7 +33,7 @@ public class UserDao {
         User user = new User();
         user.setId(rs.getString("id"));
         user.setName(rs.getString("name"));
-        user.setName(rs.getString("password"));
+        user.setPassword(rs.getString("password"));
 
         rs.close();
         ps.close();
--- src/test/java/com/tobyspring/tobyspring/MySQLConnectionTest.java
+++ src/test/java/com/tobyspring/tobyspring/MySQLConnectionTest.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 
 public class MySQLConnectionTest {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/springbook?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/springbook?useSSL=false&serverTimezone=UTC&URIEncoding=UTF-8";
     private static final String USER = "root";
     private static final String PASSWORD = "1234";
 
